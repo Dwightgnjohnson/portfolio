47c36926094eccafb8c8f107c585fa6672b1d6c9
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-10-06 18:20:57 -0500

    testing if header and footers will work on mobile

--- config.rb
+++ config.rb
@@ -47,11 +47,14 @@
 #   end
 # end
 
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+
 
 # Build-specific configuration
 configure :build do
@@ -65,7 +68,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
